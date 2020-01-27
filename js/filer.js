770e64bce72a51404ce58cd6107fe306ceeecfb2
eugene <eugsokolov@gmail.com>
2020-01-27 17:07:01 -0500

    port site

---
+++ js/filer.js
@@ -0,0 +1,12 @@
+// create a python script to save info?
+//
+// perhaps try smtpjs.com
+//let emails = ['eugsokolov@gmail.com'];
+
+function saveForm(form){
+// form.name.value
+// form.email.value
+// form.comments.value
+alert('Thank you for your input ' + form.name.value + ' !');
+
+}
